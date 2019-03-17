c20c4a92ff4fbfe3269ee67aed16536647b0bd72
sa <sparsebarse@spamhereplease.com>
2019-03-16 23:02:49 -0400

    add cname, gh pages command

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Link } from "@reach/router";
 import Headroom from "react-headroom";
 
+import "./Header.css";
+
 export default () => (
   <Headroom>
     <div className="container">
       <nav className="head">
         <div>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img
+              alt="Game Workers Unite"
+              className="header-logo-image"
+              src="/images/gwu-mag-logo.svg"
+            />
+            <span className="header-after-logo">GDC 2019 Zine</span>
+          </Link>
         </div>
         <div className="head-right">
           <a href="https://gameworkersunite.org">GWU International</a>
