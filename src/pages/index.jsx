038e89af60fe9816dedea0306f0ca79fa67b99cb
Left Burner <lefttheburneron@gmail.com>
2019-03-18 03:10:17 -0400

    Trying the metadata thing again...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,9 +7,14 @@ import DownloadZines from "../containers/DownloadZines";
 
 import "./index.css";
 
-export default withRouteData(({ openingStatement }) => (
+export default withRouteData(({ openingStatement, title, type }) => (
   <>
-    <Head title={`GWU GDC 2019 Zine`} />
+    <Head title={`GWU GDC 2019 Zine`}>
+      <meta property="og:title" content={`${type === 'unionfaqs' ? 'UnionFAQs: ' : ''}${title}`} />
+      <meta property="og:description" content="Read the latest zine from Game Workers Unite!" />
+      <meta property="og:image" content="/images/isabelle-clearbg.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
     <div className="intro-container">
       <img className="intro-image" src="images/isabelle-clearbg-inlined.svg" />
       <div dangerouslySetInnerHTML={{ __html: openingStatement.contents }} />
