ad4deef13d59b3d1b82aa7de7997b910a6c0849d
Left Burner <lefttheburneron@gmail.com>
2019-11-09 16:30:39 -0500

    Fix null prevPages issue.

--- src/containers/PrevNextLinks.jsx
+++ src/containers/PrevNextLinks.jsx
@@ -4,27 +4,33 @@ import { Link, Location } from "@reach/router";
 import { getLanguageFromPathname } from "../utils";
 import "./PrevNextLinks.css";
 
-export default ({ prevPages, nextPages }) => (
-  <Location>
-    {({ location }) => {
-      const { pathname } = location;
-      const languageCode = getLanguageFromPathname(pathname);
-      const prevPage = prevPages[languageCode];
-      const nextPage = nextPages[languageCode];
-      return (
-        <div className="prev-next-wrapper">
-          {prevPage ? (
-            <Link to={prevPage.route}>
-              {"<"} {prevPage.name}
-            </Link>
-          ) : null}
-          {nextPage ? (
-            <Link to={nextPage.route}>
-              {nextPage.name} {">"}
-            </Link>
-          ) : null}
-        </div>
-      );
-    }}
-  </Location>
-);
+export default ({ prevPages, nextPages }) => {
+  if (!prevPages) {
+    // not on a sequence page
+    return null;
+  }
+  return (
+    <Location>
+      {({ location }) => {
+        const { pathname } = location;
+        const languageCode = getLanguageFromPathname(pathname);
+        const prevPage = prevPages[languageCode];
+        const nextPage = nextPages[languageCode];
+        return (
+          <div className="prev-next-wrapper">
+            {prevPage ? (
+              <Link to={prevPage.route}>
+                {"<"} {prevPage.name}
+              </Link>
+            ) : null}
+            {nextPage ? (
+              <Link to={nextPage.route}>
+                {nextPage.name} {">"}
+              </Link>
+            ) : null}
+          </div>
+        );
+      }}
+    </Location>
+  );
+};
