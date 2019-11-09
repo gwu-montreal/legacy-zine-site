489c0853c490c9bdcb04304397d704fbae5eac13
Left Burner <lefttheburneron@gmail.com>
2019-11-09 14:49:17 -0500

    Fallback to english for language.

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-import assert from "assert";
-
 // we assume all language codes are 2 characters long.
 // if this changes we will need to update our logic.
 
@@ -7,7 +5,11 @@ const languageList = ["en", "fr"];
 
 export function getLanguageFromPathname(pathname) {
   const code = pathname.slice(1, 3);
-  assert(languageList.indexOf(code) !== -1);
+  if (languageList.indexOf(code) === -1) {
+    console.warn(`Unrecognized language code in pathname (${pathname})`);
+    // fallback to English
+    return "en";
+  }
   return code;
 }
 
