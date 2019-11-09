836f8b85c4756a24290c651f3ec29fa86cf26d97
Left Burner <lefttheburneron@gmail.com>
2019-11-09 16:48:14 -0500

    Make redirects work in Firefox.

--- src/containers/Redirect.jsx
+++ src/containers/Redirect.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import { withRouteData, Head } from "react-static";
 
 export default withRouteData(
-  ({ redirectPath }) => (
-    <React.Fragment>
-      <Head>
-        <meta
-          httpEquiv="refresh"
-          content={`0; url=${redirectPath}`}
-        />
-      </Head>
-      <div>Redirecting...</div>
-    </React.Fragment>
-  )
+  class extends React.PureComponent {
+    componentDidMount() {
+      // for Firefox which disabled meta http-equiv redirects
+      window.location.pathname = this.props.redirectPath;
+    }
+
+    render() {
+      const { redirectPath } = this.props;
+      return (
+        <React.Fragment>
+          <Head>
+            <meta
+              httpEquiv="refresh"
+              content={`0; url=${redirectPath}`}
+            />
+          </Head>
+          <div>Redirecting...</div>
+        </React.Fragment>
+      );
+    }
+  }
 );
