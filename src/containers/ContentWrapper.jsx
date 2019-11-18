e0a93ef08773cb3d5e0f0d6cf13439f8010b7a96
Left Burner <lefttheburneron@gmail.com>
2019-11-17 19:50:29 -0500

    Content description based on first paragraph.

--- src/containers/ContentWrapper.jsx
+++ src/containers/ContentWrapper.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouteData, Head } from "react-static";
 import i18next from "i18next";
+import $ from "cheerio";
 
 import Clearfix from "./Clearfix";
 import Footer from "./Footer";
@@ -20,41 +21,48 @@ export default withRouteData(
     headerClassName = "",
     contentClassName = "",
     children
-  }) => (
-    <React.Fragment>
-      <Head
-        title={`${i18next.t("site_title")} - ${title}`}
-        meta={[
-          { name: "description", content: i18next.t("meta.description") },
-          {
-            property: "og:title",
-            content: `${type === "unionfaqs" ? i18next.t("unionfaqs_prefix") + " " : ""}${title}`
-          },
-          { property: "og:description", content: i18next.t("meta.description") }
-        ]}
-      />
-      <div style={{ width: "100%" }}>
-        <div style={{ width: "100%", position: "relative" }}>
-          <div className="nav_sidebar">
-            <TableOfContents />
+  }) => {
+    const description = $(`<div>${contents}</div>`)
+      .find('p')
+      .first()
+      .text()
+      .replace(/\n/g, ' ');
+    return (
+      <React.Fragment>
+        <Head
+          title={`${i18next.t("site_title")} - ${title}`}
+          meta={[
+            { name: "description", content: description },
+            {
+              property: "og:title",
+              content: `${type === "unionfaqs" ? i18next.t("unionfaqs_prefix") + " " : ""}${title}`
+            },
+            { property: "og:description", content: description }
+          ]}
+        />
+        <div style={{ width: "100%" }}>
+          <div style={{ width: "100%", position: "relative" }}>
+            <div className="nav_sidebar">
+              <TableOfContents />
+            </div>
           </div>
+          <div className={`page ${containerClassName}`}>
+            {header ? (
+              header(title)
+            ) : (
+              <h2 className={headerClassName}>{title}</h2>
+            )}
+            {children || (
+              <div
+                className={`page-content ${contentClassName}`}
+                dangerouslySetInnerHTML={{ __html: contents }}
+              />
+            )}
+            <Clearfix />
+          </div>
+          <Footer prevPages={prevPages} nextPages={nextPages} />
         </div>
-        <div className={`page ${containerClassName}`}>
-          {header ? (
-            header(title)
-          ) : (
-            <h2 className={headerClassName}>{title}</h2>
-          )}
-          {children || (
-            <div
-              className={`page-content ${contentClassName}`}
-              dangerouslySetInnerHTML={{ __html: contents }}
-            />
-          )}
-          <Clearfix />
-        </div>
-        <Footer prevPages={prevPages} nextPages={nextPages} />
-      </div>
-    </React.Fragment>
-  )
+      </React.Fragment>
+    );
+  }
 );
